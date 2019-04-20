69564a4212c8caf35b9055f231b2f43918fc9b70
benborgers <borgersbenjamin@gmail.com>
2019-04-20 19:01:42 -0400

    refactor to use distinct GraphQL query

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,29 @@ import React from 'react'
 import Layout from '../components/layout'
 import Intro from '../components/intro'
 import Block from '../components/block'
+import Heading from '../components/heading'
 import bigIdea from '../util/bigIdea'
 import { graphql } from 'gatsby'
 
-export default ({ data }) => {
-  const nodes = data.allAirtable.edges;
-  let bigIdeaNumbers = [];
-  for(let node of nodes) {
-    bigIdeaNumbers.push(node.node.data.Big_Idea);
-  }
-  const uniqueBigIdeaNumbers = Array.from(new Set(bigIdeaNumbers));
-  
-  return (
-    <Layout>
-      <Intro>
-        <p>
-          This study guide focuses on studying through active recall — that is, studying by trying to recall information, instead of just reading notes.
-        </p>
-        <p>
-          It's organized into the Big Ideas and Enduring Understandings put out by the College Board for this exam. 
-        </p>
-      </Intro>
-      
-      {uniqueBigIdeaNumbers.map((number, i) => (
-        <Block key={i} to={'big-idea-' + number} text={
-          `Big Idea ${number}: ${bigIdea[number].statement}`
-        }/>
-      ))}
-      
-    </Layout>
-  )
-}
+export default ({ data }) => (
+  <Layout>
+    <Intro>
+      <p>
+        This study guide focuses on studying through active recall — that is, studying by trying to recall information, instead of just reading notes.
+      </p>
+      <p>
+        It's organized into the Big Ideas and Enduring Understandings put out by the College Board for this exam. 
+      </p>
+    </Intro>
+    
+    {data.allAirtable.distinct.map((number, i) => (
+      <Block key={i} to={'big-idea-' + number} text={
+        `Big Idea ${number}: ${bigIdea[number].statement}`
+      }/>
+    ))}
+    
+  </Layout>
+)
 
 export const query = graphql`
   query {
@@ -42,13 +34,7 @@ export const query = graphql`
         Publish: { eq: true }
       }
     }) {
-      edges {
-        node {
-          data {
-            Big_Idea
-          }
-        }
-      }
+      distinct(field: data___Big_Idea)
     }
   }
 `
