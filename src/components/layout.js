657bb1e63be7d374bb5c2baacf22e737c04c83ea
benborgers <borgersbenjamin@gmail.com>
2019-04-20 17:23:39 -0400

    build layout

---
+++ src/components/layout.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { css, Global } from '@emotion/core'
+import { colors } from '../util/theme.js'
+import { Helmet } from 'react-helmet'
+
+export default ({ children }) => {
+  const siteMetadata = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            isWorkInProgress
+          }
+        }
+      }
+    `
+  ).site.siteMetadata
+  
+  return (
+    <div>
+      <Helmet>
+        <title>{siteMetadata.title}</title>
+        <meta name="description" content={siteMetadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+    
+      <Global
+        styles={css`
+          * {
+            font-family: -apple-system, BlinkMacSystemFont, Roboto, system-ui, sans-serif;
+            color: ${colors.gray};
+            font-size: 16px;
+            font-weight: 400;
+            margin: 0;
+            padding: 0;
+            outline: none;
+            background: transparent;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+      />
+    
+      <header
+        css={css`
+          background-color: ${colors.lightgray};
+          padding: 24px;
+        `}>
+        <p
+          css={css`
+            color: ${colors.black};
+            font-weight: 500;
+          `}>{siteMetadata.title}</p>
+      </header>
+      
+      <main
+        css={css`
+          padding: 24px;
+      `}>
+        {children}
+      </main>
+      
+      <footer
+        css={css`
+          background-color: ${colors.lightgray};
+          padding: 24px;
+      `}>
+        <p>Made by <a href="https://instagram.com/bborgers">Ben Borgers</a> — Good luck 🌻</p>
+      </footer>
+    </div>
+  )
+}
---
+++ src/util/theme.js
@@ -0,0 +1,10 @@
+module.exports = {
+  colors: {
+    main: `rgba(78, 238, 148, 1)`,
+    lightgray: `rgba(243, 255, 248, 1)`,
+    
+    // text colors
+    black: `rgba(6, 37, 20, 1)`,
+    gray: `rgba(104, 113, 108, 1)`
+  }
+}
