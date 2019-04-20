10e38c6a79cdc34c944361d9b4a747acddc685bd
benborgers <borgersbenjamin@gmail.com>
2019-04-20 18:34:57 -0400

    update Big Ideas list

--- src/util/bigIdea.js
+++ src/util/bigIdea.js
@@ -1,14 +1,31 @@
 module.exports = {
   1: {
-    statement: `The process of evolution drives the diversity and unity of life.`
+    statement: `The process of evolution drives the diversity and unity of life.`,
+    A: `Change in the genetic makeup of a population over time is evolution.`,
+    B: `Organisms are linked by lines of descent from common ancestry.`,
+    C: `Life continues to evolve within a changing environment.`,
+    D: `The origin of living systems is explained by natural processes.`
   },
   2: {
-    statement: `Biological systems utilize free energy and molecular building blocks to grow, to reproduce, and to maintain dynamic homeostasis.`
+    statement: `Biological systems utilize free energy and molecular building blocks to grow, to reproduce, and to maintain dynamic homeostasis.`,
+    A: `Growth, reproduction and maintenance of the organization of living systems require free energy and matter.`,
+    B: `Growth, reproduction and dynamic homeostasis require that cells create and maintain internal environments that are different from their external environments.`,
+    C: `Organisms use feedback mechanisms to regulate growth and reproduction, and to maintain dynamic homeostasis.`,
+    D: `Growth and dynamic homeostasis of a biological system are influenced by changes in the system’s environment.`,
+    E: `Many biological processes involved in growth, reproduction and dynamic homeostasis include temporal regulation and coordination.`
   },
   3: {
-    statement: `Living systems store, retrieve, transmit, and respond to information essential to life processes.`
+    statement: `Living systems store, retrieve, transmit, and respond to information essential to life processes.`,
+    A: `Heritable information provides for continuity of life.`,
+    B: `Expression of genetic information involves cellular and molecular mechanisms.`,
+    C: `The processing of genetic information is imperfect and is a source of genetic variation.`,
+    D: `Cells communicate by generating, transmitting and receiving chemical signals.`,
+    E: `Transmission of information results in changes within and between biological systems.`
   },
   4: {
-    statement: `Living systems store, retrieve, transmit, and respond to information essential to life processes.`
+    statement: `Living systems store, retrieve, transmit, and respond to information essential to life processes.`,
+    A: `Interactions within biological systems lead to complex properties.`,
+    B: `Competition and cooperation are important aspects of biological systems.`,
+    C: `Naturally occurring diversity among and between components within biological systems affects interactions with the environment.`
   }
 }
