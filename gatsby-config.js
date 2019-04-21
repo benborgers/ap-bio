3724fff812cd083a93eae8513732bfbcc78ffd13
benborgers <borgersbenjamin@gmail.com>
2019-04-20 23:52:07 -0400

    add to siteMetadata description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Bio AP Study Guide`,
-    description: `A study guide for the Biology AP Test, focused on studying through active recall by quizzing yourself.`,
+    description: `A study guide for the Biology AP Test, focused on studying through active recall by quizzing yourself instead of reading notes.`,
     isWorkInProgress: true
   },
   plugins: [
