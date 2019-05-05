e3f385b0ba63a73aef716bc2ee4c1b2da0ec6c64
benborgers <borgersbenjamin@gmail.com>
2019-05-05 15:46:48 -0400

    add lightGray to colors in theme

--- src/util/theme.js
+++ src/util/theme.js
@@ -3,6 +3,7 @@ module.exports = {
     main: `rgba(78, 189, 148, 1)`,
     middlemain: `rgba(195, 214, 207, 1)`,
     lightmain: `rgba(243, 255, 248, 1)`,
+    lightgray: `rgba(231, 236, 234)`,
     
     // text colors
     black: `rgba(6, 37, 20, 1)`,
