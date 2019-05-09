5e30c812903fe135d9241de060c13fc7c89b076c
Ben Borgers <borgersbenjamin@gmail.com>
2019-05-09 13:54:28 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Intro from '../components/intro'
 import Block from '../components/block'
 import bigIdea from '../util/bigIdea'
 import { graphql } from 'gatsby'
